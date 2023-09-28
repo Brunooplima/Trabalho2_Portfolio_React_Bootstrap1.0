ef0cf87fe9ec937dd8502a398569605d0a8c4c2f
Bruno Lima <138326567+Brunooplima@users.noreply.github.com>
2023-09-28 01:28:12 -0300

    tentativas

---
+++ portfolio-app/src/components/Header.js
@@ -0,0 +1,32 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Bruno Lima</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/home">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/projects">
+              Projetos
+            </Nav.Link>
+            <Nav.Link as={Link} to="/clients">
+              Parceiros
+            </Nav.Link>
+            <Nav.Link as={Link} to="contact">
+              Contatos
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default Header;
